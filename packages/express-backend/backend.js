27fa60a05327f372b8aed175572158688b2f6400
smbock42 <smbock42@gmail.com>
2023-10-08 18:46:37 -0700

    add user api works

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -43,6 +43,11 @@ const findUserById = (id) =>
         .find( (user) => user['id'] === id);
 
  
+const addUser = (user) => {
+    users['users_list'].push(user);
+    return user;
+}
+        
 
 app.use(express.json());
 
@@ -70,6 +75,12 @@ app.get('/users/:id', (req, res) => {
     } else {
         res.send(result);
     }
+});
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
 });
 
 
